fd2b8a7ec1d32f32263c07d10681dd57f6038ab6
Debdeep Panja <154681535+dpanja1234@users.noreply.github.com>
2026-07-07 13:00:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,27 @@
-import React from "react";
+import React from 'react'
 
 const Hero = () => {
-  const scrollToPricing = () => {
-    const pricingSection = document.getElementById("pricing");
-
-    if (pricingSection) {
-      pricingSection.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  };
-
   return (
-    <section className="hero">
-      <div className="content">
-        <div className="title">
-          <h1>Lift</h1>
-          <h1>Sweat</h1>
-          <h1>Conquer</h1>
-        </div>
+    <section className='hero'>
+        <div className="content">
 
+            <div className="title">
+                <h1>Lift</h1>
+                <h1>Sweat</h1>
+                <h1>Conquer</h1>
+            </div>
+        
         <div className="sub-title">
-          <p>Begin Your Fitness Journey</p>
-          <p>Unlock Your Power</p>
+            <p>Begin Your Fitness Journey </p>
+            <p>Unlock Your Power</p>
         </div>
-
         <div className="buttons">
-          <button onClick={scrollToPricing}>
-            Start Your Journey
-          </button>
-
-          <button onClick={scrollToPricing}>
-            Discover Your Plan
-          </button>
+            <button> Start Your Journey </button>
+            <button> Discover your plan</button>
         </div>
-      </div>
-    </section>
-  );
-};
+        </div>
+        </section>
+  )
+}
 
-export default Hero;
+export default Hero
